8d5afe7ca4fe05d8a48ce7578809e1f86e04dc17
RiRyCasals <me@example.com>
2021-05-28 09:41:22 +0900

    added error.html, error.js in error dir

---
+++ error/error.js
@@ -0,0 +1,17 @@
+
+// xxx, cannot read property
+//
+// alert("There will be an error")
+// [1, 2].forEach(alert)
+
+
+// ooo
+alert("There will be an error");
+[1, 2].forEach(alert);
+
+
+// comment out
+
+/*
+ * comment out
+ */
