1eb53793a323741a62e7d140fd53f55097a1103e
RiRyCasals <me@example.com>
2021-05-28 09:43:04 +0900

    added variable.html and js in variable dir

---
+++ variable/variable.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// let is modern, var is traditional
+let message;
+message = 'Hello';
+
+let test = 'test';
+
+// xxx
+// let user = 'riry', let age = 111;
+
+let user = 'riry';
+let age = 111;
+
+
+const COLOR_RED = '#F00';
+const COLOR_GREEN = '#0F0';
+const COLOR_BLUE = '#00F';
+let color = COLOR_BLUE
+alert(color);
+
+
+// task
+let admin;
+let name;
+name = 'Jhon';
+admin = name;
+alert(admin);
